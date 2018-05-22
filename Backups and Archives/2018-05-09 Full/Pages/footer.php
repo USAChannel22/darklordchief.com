837e3502474d54e5540759cd6cec36f72f93fd8a
stere0123 <stere0123.b@gmail.com>
2018-05-21 21:53:45 -0400

    Initial Conversion
    Initial conversion from PHP source to GitHub Pages compatible static HTML/CSS/JS

--- Backups and Archives/2018-05-09 Full/Pages/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="panel panel-default panel-body center">
-    <a href="http://DarkLordChief.USAChannel22.Local/Pages/index.php">
-        <div class="label label-info">&copy; 2018 DarklordChief</div>
-    </a>
-</div>
-</div>
-</body>
